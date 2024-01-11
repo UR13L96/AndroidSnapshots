130338c158ae797cf580996c2668fb20f08921e9
Uriel <uriel1396@gmail.com>
2024-01-11 09:38:52 -0600

    Aded fragments and first configuration of fragment manager

--- app/src/main/java/com/ur13l96/myapplication/MainActivity.kt
+++ app/src/main/java/com/ur13l96/myapplication/MainActivity.kt
@@ -2,10 +2,40 @@ package com.ur13l96.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.ur13l96.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+        mFragmentManager = supportFragmentManager
+        mFragmentManager.beginTransaction()
+            .add(R.id.host_fragment, profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment)
+            .commit()
+        mFragmentManager.beginTransaction()
+            .add(R.id.host_fragment, addFragment, AddFragment::class.java.name)
+            .hide(addFragment)
+            .commit()
+        mFragmentManager.beginTransaction()
+            .add(R.id.host_fragment, homeFragment, HomeFragment::class.java.name)
+            .commit()
     }
 }
