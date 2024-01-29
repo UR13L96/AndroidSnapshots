4cf449269760a875b063c24028bfbcf22d198c0b
Uriel <uriel1396@gmail.com>
2024-01-29 09:56:34 -0600

    Added functionality to inflate recycler view with RealTimeDatabase info

--- app/src/main/java/com/ur13l96/myapplication/HomeFragment.kt
+++ app/src/main/java/com/ur13l96/myapplication/HomeFragment.kt
@@ -6,16 +6,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
+import com.google.firebase.database.FirebaseDatabase
+import com.ur13l96.myapplication.databinding.FragmentHomeBinding
 import com.ur13l96.myapplication.databinding.ItemSnapshotBinding
 
 
 class HomeFragment : Fragment() {
+    private lateinit var mFirebaseAdapter: FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>
+    private lateinit var mBinding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val query = FirebaseDatabase.getInstance().reference.child("snapshots")
+        val options = FirebaseRecyclerOptions.Builder<Snapshot>().setQuery(query, Snapshot::class.java)
+            .build()
+
+        mFirebaseAdapter = object : FirebaseRecyclerAdapter<Snapshot, SnapshotHolder>(options) {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SnapshotHolder {
+                TODO("Not yet implemented")
+            }
+
+            override fun onBindViewHolder(holder: SnapshotHolder, position: Int, model: Snapshot) {
+                TODO("Not yet implemented")
+            }
+        }
     }
 
     inner class SnapshotHolder(view: View): RecyclerView.ViewHolder(view) {
